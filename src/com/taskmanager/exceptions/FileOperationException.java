938219db79a2369c146ffed638f79331a79f95b7
amandx36 <xamandeep360@gmail.com>
2025-11-04 23:39:43 +0530

    updating the file structure dude

--- src/com/taskmanager/exceptions/FileOperationException.java
+++
@@ -1,8 +0,0 @@
-package com.taskmanager.exceptions;
-
-public class FileOperationException extends Exception{
-    public FileOperationException (String message ){
-        // calling the parent const through super so i can save the property error in exception class and use the function normally e.getMessage() otherwise i got the null error !!! 
-        super(message);
-    }
-}
---
+++ src/com/taskmanager/utils/ConsoleColors.java
@@ -0,0 +1,19 @@
+package com.taskmanager.utils;
+
+public class ConsoleColors {
+    // using the word final means unchangable 
+    // static :) means use when it when we dont want to create the object of it !!! 
+    public static final String RESET = "\033[0m";
+    public static final String RED = "\033[0;31m";
+    public static final String GREEN = "\033[0;32m";
+    public static final String YELLOW = "\033[0;33m";
+    public static final String BLUE = "\033[0;34m";
+    public static final String PURPLE = "\033[0;35m";
+    public static final String CYAN = "\033[0;36m";
+    
+    public static final String RED_BOLD = "\033[1;31m";
+    public static final String GREEN_BOLD = "\033[1;32m";
+    public static final String YELLOW_BOLD = "\033[1;33m";
+    public static final String BLUE_BOLD = "\033[1;34m";
+    public static final String CYAN_BOLD = "\033[1;36m";
+}
