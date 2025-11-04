9cbfce730c255e31b74c503e99e8a62d64b83909
amandx36 <xamandeep360@gmail.com>
2025-11-04 23:00:40 +0530

    makking my own data type with the help of  making class

---
+++ src/com/taskmanager/models/Task.java
@@ -0,0 +1,97 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Task {
+
+    // public class making my own data- type using enum
+    public enum TaskStatus {
+        PENDING, COMPLETED, IN_PROGRESS;
+    }
+
+    public enum Priority {
+        LOW, MEDIUM, HEIGHT, URGENT;
+    }
+
+    // making my own data-type using class !!
+
+    private int id;
+    private String title;
+    private String description;
+    private TaskStatus status;
+    private Priority priority;
+    private LocalDate dueDate;
+    private LocalDate createdAt;
+    private int projectId;
+
+    // makking the constructor for that !!!!
+    public Task(int id, String title, String description, Priority priority, LocalDate dueDate, int projectId) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = TaskStatus.PENDING;
+        this.priority = priority;
+        this.dueDate = dueDate;
+        this.createdAt = LocalDate.now();
+        this.projectId = projectId;
+    }
+
+    // making the getter and setter for getting and setting the value locally dude
+    // !!!
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public int getProjectId() {
+        return projectId;
+    }
+
+    // over-writting the parent method and making my own method which is also
+    // written in the parent class !!!
+    @Override
+    public String toString() {
+
+        DateTimeFormatter formater = DateTimeFormatter.ofPattern("dd-mm-yyy");
+        return String.format("ID: %d | %s | %s | Priority: %s | Due: %s |Stauts: %s ", id, title, description, priority,
+                dueDate.format(formater), status);
+    }
+}
